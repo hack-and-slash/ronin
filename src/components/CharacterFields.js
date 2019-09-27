ccd596d460c4310f18c35214eba503470b71ce26
Victor Miguez <victorcineis@gmail.com>
2019-09-27 18:43:13 -0300

    Fix fields position

--- src/components/CharacterFields.js
+++ src/components/CharacterFields.js
@@ -7,50 +7,54 @@ import TextInput from './Fields/TextInput';
 import styles from './CharacterFields.module.css';
 
 const CharacterFields = () => (
-  <>
-  <div className={styles.fields}>
-    <TextInput label="Nome:" name="nome" />
-    <TextInput label="Aparência:" name="aparencia" />
-    <TextInput label="Técnica:" name="tecnica" />
-    <TextInput label="Combate:" name="combate" />
-    <TextInput label="Bloqueio:" name="bloqueio" />
-    <TextInput label="Família:" name="familia" />
-    <TextInput label="Pesadelo:" name="pesadelo" />
-    <TextInput label="Cicatriz:" name="cicatriz" />
-    <TextInput label="Significado:" name="significado" />
-  </div>
-  <div>
-    <label>Histórico</label>
-    <Field name="historico" component="textarea" />
-  </div>
-  <div>
-    <label>Reputação</label>
-    <Checkbox name="reputacao" type="checkbox" value="1" />
-    <Checkbox name="reputacao" type="checkbox" value="2" />
-    <Checkbox name="reputacao" type="checkbox" value="3" />
-    <Checkbox name="reputacao" type="checkbox" value="4" />
-    <Checkbox name="reputacao" type="checkbox" value="5" />
-    <Checkbox name="reputacao" type="checkbox" value="6" />
-  </div>
-  <div>
-    <label>Compaixão</label>
-    <Checkbox name="compaixao" type="checkbox" value="1" />
-    <Checkbox name="compaixao" type="checkbox" value="2" />
-    <Checkbox name="compaixao" type="checkbox" value="3" />
-    <Checkbox name="compaixao" type="checkbox" value="4" />
-    <Checkbox name="compaixao" type="checkbox" value="5" />
-    <Checkbox name="compaixao" type="checkbox" value="6" />
-  </div>
-  <div>
-    <label>Determinação</label>
-    <Checkbox name="determinacao" type="checkbox" value="1" />
-    <Checkbox name="determinacao" type="checkbox" value="2" />
-    <Checkbox name="determinacao" type="checkbox" value="3" />
-    <Checkbox name="determinacao" type="checkbox" value="4" />
-    <Checkbox name="determinacao" type="checkbox" value="5" />
-    <Checkbox name="determinacao" type="checkbox" value="6" />
-  </div>
-  </>
+  <section className={styles.characterFields}>
+    <div className={styles.backgroundFields}>
+      <div className={styles.fields}>
+        <TextInput label="Nome:" name="nome" />
+        <TextInput label="Aparência:" name="aparencia" />
+        <TextInput label="Técnica:" name="tecnica" />
+        <TextInput label="Combate:" name="combate" />
+        <TextInput label="Bloqueio:" name="bloqueio" />
+        <TextInput label="Família:" name="familia" />
+        <TextInput label="Pesadelo:" name="pesadelo" />
+        <TextInput label="Cicatriz:" name="cicatriz" />
+        <TextInput label="Significado:" name="significado" />
+      </div>
+      <div className={styles.history}>
+        <label className={styles.historyLabel}>Histórico</label>
+        <Field name="historico" component="textarea" className={styles.historyField} />
+      </div>
+    </div>
+    <div className={styles.checkboxes}>
+      <div>
+        <label>Reputação:</label>
+        <Checkbox name="reputacao" type="checkbox" value="1" />
+        <Checkbox name="reputacao" type="checkbox" value="2" />
+        <Checkbox name="reputacao" type="checkbox" value="3" />
+        <Checkbox name="reputacao" type="checkbox" value="4" />
+        <Checkbox name="reputacao" type="checkbox" value="5" />
+        <Checkbox name="reputacao" type="checkbox" value="6" />
+      </div>
+      <div>
+        <label>Compaixão:</label>
+        <Checkbox name="compaixao" type="checkbox" value="1" />
+        <Checkbox name="compaixao" type="checkbox" value="2" />
+        <Checkbox name="compaixao" type="checkbox" value="3" />
+        <Checkbox name="compaixao" type="checkbox" value="4" />
+        <Checkbox name="compaixao" type="checkbox" value="5" />
+        <Checkbox name="compaixao" type="checkbox" value="6" />
+      </div>
+      <div>
+        <label>Determinação:</label>
+        <Checkbox name="determinacao" type="checkbox" value="1" />
+        <Checkbox name="determinacao" type="checkbox" value="2" />
+        <Checkbox name="determinacao" type="checkbox" value="3" />
+        <Checkbox name="determinacao" type="checkbox" value="4" />
+        <Checkbox name="determinacao" type="checkbox" value="5" />
+        <Checkbox name="determinacao" type="checkbox" value="6" />
+      </div>
+    </div>
+  </section>
 );
 
 export default CharacterFields;
