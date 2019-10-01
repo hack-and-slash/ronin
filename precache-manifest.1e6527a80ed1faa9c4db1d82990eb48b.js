d563b13724bf3447b20cf4df659267767ad2cd04
Victor Miguez <victorcineis@gmail.com>
2019-10-01 20:48:24 -0300

    Updates

---
+++ precache-manifest.1e6527a80ed1faa9c4db1d82990eb48b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b205106f1bca9f0c07e3418e5e4e3a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7fed08b8a3c50edcf62",
+    "url": "/static/css/main.e79f460f.chunk.css"
+  },
+  {
+    "revision": "c2be0886a2ddb77f52ad",
+    "url": "/static/js/2.296d5600.chunk.js"
+  },
+  {
+    "revision": "a7fed08b8a3c50edcf62",
+    "url": "/static/js/main.74f4c18e.chunk.js"
+  },
+  {
+    "revision": "5ed0464c9aa582732d86",
+    "url": "/static/js/runtime-main.1cfae40a.js"
+  },
+  {
+    "revision": "442b567cad96364a2004a1a9433fa84a",
+    "url": "/static/media/clan-1.442b567c.jpg"
+  },
+  {
+    "revision": "05624a6a36c15afe5817d9d5af249904",
+    "url": "/static/media/clan-2.05624a6a.jpg"
+  },
+  {
+    "revision": "db4f29d1da633c8fdbf1d39670216e84",
+    "url": "/static/media/clan-3.db4f29d1.jpg"
+  },
+  {
+    "revision": "3ec186ae0ca3808a79105d2152416ee6",
+    "url": "/static/media/clan-4.3ec186ae.jpg"
+  }
+]);
